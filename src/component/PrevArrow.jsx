0d80164246930a80583fd351421b1139c8595d5c
DevvEmeka <devemeka@outlook.com>
2024-04-04 14:32:26 +0100

    made adjustment in next and previous arrows

--- src/component/PrevArrow.jsx
+++ src/component/PrevArrow.jsx
@@ -3,7 +3,7 @@ import {BsChevronLeft} from "react-icons/bs"
 
 const PrevArrow = ({ onClick }) => {
   return (
-    <div className='absolute laptop:left-[-357px] laptop:-top-[-355px] left-[100px] -top-[-500px] mobile:-top-[-500px]' onClick={onClick}>
+    <div className='absolute laptop:left-[-357px] laptop:-top-[-355px] left-[192px] -top-[-500px] mobile:-top-[-500px]' onClick={onClick}>
         <div className='bg-[#000] border-white border-[1.5px] h-[34px] w-[34px] rounded-full grid place-items-center cursor-pointer'>
             <BsChevronLeft className='fill-[#ffe369]' />
         </div>
