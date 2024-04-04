ca3e604403d93beb0fea19b8c97040114087d6f8
DevvEmeka <devemeka@outlook.com>
2024-04-04 14:37:32 +0100

    made additional adjustment in next and previous arrows

--- src/component/NextArrow.jsx
+++ src/component/NextArrow.jsx
@@ -3,7 +3,7 @@ import {BsChevronRight} from "react-icons/bs"
 
 const NextArrow = ({ onClick }) => {
   return (
-    <div className='absolute laptop:left-[-283px] laptop:-top-[-355px] left-[258px] -top-[-500px]' onClick={onClick}>
+    <div className='absolute laptop:left-[-283px] laptop:-top-[-355px] left-[280px] -top-[-500px]' onClick={onClick}>
         <div className='bg-[#000] border-white border-[1.5px] h-[34px] w-[34px] rounded-full grid place-items-center cursor-pointer'>
             <BsChevronRight className='fill-[#ffe369]' />
         </div>
