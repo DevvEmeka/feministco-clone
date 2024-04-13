dc29c97b7db803e5d286e1a51f82f19070a6db12
DevvEmeka <devemeka@outlook.com>
2024-04-13 11:49:32 +0100

    make fixes on footer component

--- src/files/Finances.jsx
+++ src/files/Finances.jsx
@@ -8,7 +8,7 @@ const Finances = () => {
       <h2 className="text-[1.6em] laptop:text-[3.8em] font-sangbleu mb-[30px] font-semibold laptop:font-bold ">
         What We Have Done.
       </h2>
-      <p className="text-[1.18em] laptop:text-[1.5em] font-sans mb-[30px] laptop:leading-9 leading-7">
+      <p className="text-[1em] laptop:text-[1.5em] font-sans mb-[30px] laptop:leading-9 leading-7">
         Here is a summary of the total funds we received and disbursed (in
         Naira) as of <br /> October 22nd, 2020: Total received = ₦147,855,788.28
         (includes donations in USD, <br /> CAD, GBP, EUR, GHS, KES, and BTC)
@@ -21,28 +21,28 @@ const Finances = () => {
         </div>
         <div className="laptop:flex-grow-0 laptop:flex-shrink-0 laptop:w-[52%] laptop:max-w-[52%] font-sans">
           <ul className="m-0 p-0 list-none grid gap-[0.5rem]">
-            <li className="before-ti-star laptop:text-[1.4em] text-[1.1em] font-normal laptop:leading-7">Total disbursed = ₦60,403,235.00</li>
-            <li className="before-ti-star laptop:text-[1.4em] text-[1.1em] font-normal laptop:leading-7">Total left = ₦87,452,553.28</li>
+            <li className="before-ti-star laptop:text-[1.4em] text-[0.95em font-normal laptop:leading-7">Total disbursed = ₦60,403,235.00</li>
+            <li className="before-ti-star laptop:text-[1.4em] text-[0.95em font-normal laptop:leading-7">Total left = ₦87,452,553.28</li>
           </ul>
           <p className="leading-6 text-[0.95em] font-bold px-0 py-[20px] laptop:py-[40px] laptop:font-bold laptop:text-[1.2rem] laptop:leading-8">
             The remaining funds will be channeled through the following
             initiatives/organizations:
           </p>
           <ul className="m-0 p-0 list-none grid gap-[0.5rem]">
-            <li className="before-ti-star laptop:text-[1.4em] text-[1.1em] font-normal laptop:leading-7">
+            <li className="before-ti-star laptop:text-[1.4em] text-[0.95em font-normal laptop:leading-7">
               #EndSARSresponse (Medical) - ₦20,114,087.25
             </li>
-            <li className="before-ti-star laptop:text-[1.4em] text-[1.1em] font-normal laptop:leading-7">
+            <li className="before-ti-star laptop:text-[1.4em] text-[0.95em font-normal laptop:leading-7">
               The Legal Aid Network - ₦15,741,459.59
             </li>
-            <li className="before-ti-star laptop:text-[1.4em] text-[1.1em] font-normal laptop:leading-7">
+            <li className="before-ti-star laptop:text-[1.4em] text-[0.95em font-normal laptop:leading-7">
               Relief for victims of police brutality and families of the
               deceased - ₦40,228,174.51
             </li>
-            <li className="before-ti-star laptop:text-[1.4em] text-[1.1em] font-normal laptop:leading-7">
+            <li className="before-ti-star laptop:text-[1.4em] text-[0.95em font-normal laptop:leading-7">
               Memorial for the Fallen - ₦5,247,153.197
             </li>
-            <li className="before-ti-star laptop:text-[1.4em] text-[1.1em] font-normal laptop:leading-7">
+            <li className="before-ti-star laptop:text-[1.4em] text-[0.95em font-normal laptop:leading-7">
               #EndSARSMentalHealthSupport - ₦6,121,678.73
             </li>
           </ul>
